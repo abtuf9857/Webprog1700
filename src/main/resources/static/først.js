ab61c681b016c9187e19592197635049ac6843e2
Tufa <abtuf9857@oslomet.no>
2024-01-23 18:20:11 +0100

    siste endringer

--- src/main/resources/static/først.js
+++ src/main/resources/static/først.js
@@ -1,1 +1,21 @@
-function alert()
+function helloWorld() {
+    document.write("Hei verden")
+    //funksjonen helloWorld skriver "Hei verden"
+}
+//funksjonen alertWorld gjør at alert skal skrive "Hei verden"
+function alertWorld(){
+    alert("Hei verden")
+}
+//funksjonen tonySide har tekst som parameter og skriver det samme ut
+function toNySide(innputText){
+    document.write(innputText)
+
+}
+
+function endre(innputText){
+    alert(innputText)
+}
+
+function popup (info){
+    console.log(info)
+}
