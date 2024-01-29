1d7b0dc83ee7c6f905f781328ef3ab6a9b79cda8
Tufa <abtuf9857@oslomet.no>
2024-01-29 16:02:19 +0100

    test

---
+++ src/test/java/com/example/obligone/ObligOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
