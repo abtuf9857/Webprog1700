f38c48e153ead58bb2aa87fa685e4e1e880bd0b6
Tufa <abtuf9857@oslomet.no>
2024-01-29 19:01:35 +0100

    første dag

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,1 +1,13 @@
-.
+function billetkjøp(){
+    let billetter=[]
+
+function sjekk(){
+        const streng=document.getElementById("Antall").value;
+        const tall=Number(streng);
+        if(ikkeTall(tall)){
+            alert("Ikke et tall");
+        }
+
+    }
+}
+
