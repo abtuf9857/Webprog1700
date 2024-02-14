86ddc33951e49db6163a3d9cadc0753aedda27d0
Tufa <abtuf9857@oslomet.no>
2024-02-14 11:59:47 +0100

    siste endringer

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,13 +1,32 @@
 function billetkjøp(){
     let billetter=[]
+}
 
-function sjekk(){
-        const streng=document.getElementById("Antall").value;
-        const tall=Number(streng);
-        if(ikkeTall(tall)){
-            alert("Ikke et tall");
-        }
-
+function sjekkAntall(){
+    var Antall = document.getElementById("Antall")
+    const streng = Antall.value;
+    const tall = Number(streng);
+    if (isNaN(tall)) {
+        Antall.setCustomValidity("Ikke et tall");
+    } else {
+        Antall.setCustomValidity("")
     }
+
+    Antall.reportValidity()
+
+}
+function sjekkFornavn(){
+    var Fornavn = document.getElementById("Fornavn")
+    const streng = Fornavn.value;
+    if (typeof streng ==="string")
+    {
+        Fornavn.setCustomValidity("Ikke et navn");
+    }
+else
+    {
+        Fornavn.setCustomValidity("")
+    }
+
+    Fornavn.reportValidity()
 }
 
