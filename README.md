ced738fc1f8871131f336b9025f7c74034332853
Tufa <abtuf9857@oslomet.no>
2024-02-28 14:47:37 +0100

    cleanup git

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Webprog1700
+Oblig 1
+=======
+OsloMet brukernavn: 
+
+GitHub brukernavn: 
+
+Github repo URL: 
+
+Fullt navn: 
+
+Kort beskrivelse av applikasjon (5-10 setninger):
---
+++ src/main/java/com/example/obligone/ObligOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligOneApplication.class, args);
+	}
+
+}
